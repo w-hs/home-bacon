bf40667bffd37ce4fb53d32f4d300a5e20a742bc
Daniel <danielhardes@gmail.com>
2015-11-17 16:07:46 +0100

    erstes layout mobile

--- HomeBacon/mobile/src/main/java/de/whs/homebacon/MainActivity.java
+++ HomeBacon/mobile/src/main/java/de/whs/homebacon/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +28,13 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Spinner spinner = (Spinner) findViewById(R.id.eventSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.eventTypes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        
     }
 
 
