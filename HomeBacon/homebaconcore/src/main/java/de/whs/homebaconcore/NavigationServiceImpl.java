328b3314b6ef5cb852c336896a8d094280a1bd09
MillerDennis <miller.dennis4711@gmail.com>
2015-11-24 18:45:13 +0100

    bluetooth . 2. Versuch

--- HomeBacon/homebaconcore/src/main/java/de/whs/homebaconcore/NavigationServiceImpl.java
+++ HomeBacon/homebaconcore/src/main/java/de/whs/homebaconcore/NavigationServiceImpl.java
@@ -4,13 +4,20 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import static android.support.v4.app.ActivityCompat.startActivityForResult;
 
@@ -21,11 +28,11 @@ public class NavigationServiceImpl implements NavigationService {
 
     private BluetoothAdapter BTAdapter;
     private BroadcastReceiver mReceiver;
-    private ArrayAdapter<String> mArrayAdapter;
+    private Handler mHandler = new Handler();
 
     public static int REQUEST_BLUETOOTH = 1;
 
-    public NavigationServiceImpl (Activity activity){
+    public NavigationServiceImpl (final Activity activity){
         BTAdapter = BluetoothAdapter.getDefaultAdapter();
 
         // Phone does not support Bluetooth so let the user know and exit.
@@ -47,28 +54,62 @@ public class NavigationServiceImpl implements NavigationService {
             activity.startActivityForResult(enableBT, REQUEST_BLUETOOTH);
         }
 
-        if (BTAdapter.isEnabled()) {
-            BTAdapter.startDiscovery();
-            mReceiver = new BroadcastReceiver() {
-            public void onReceive(Context context, Intent intent) {
-                    String action = intent.getAction();
-
-                    //Finding devices
-                    if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                        // Get the BluetoothDevice object from the Intent
-                        BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                        int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
-                        // Add the name and address to an array adapter to show in a ListView
-                        String msg = device.getName() + "\n" + device.getAddress() + "\n" + rssi;
-                        //mArrayAdapter.add(msg);
-                        Log.d("BLUETOOTH", msg);
-                    }
-                }
-            };
-        };
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                scan();
+            }
+        },0,1000);
 
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         activity.registerReceiver(mReceiver, filter);
+    }
+
+    private void scan(){
+
+        if (BTAdapter.isEnabled()) {
+            /** BTAdapter.startDiscovery();
+             mReceiver = new BroadcastReceiver() {
+             public void onReceive(Context context, Intent intent) {
+             String action = intent.getAction();
+
+             //Finding devices
+             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+             // Get the BluetoothDevice object from the Intent
+             BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+             int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
+             // Add the name and address to an array adapter to show in a ListView
+             String msg = device.getName() + "\n" + device.getAddress() + "\n" + rssi;
+             //mArrayAdapter.add(msg);
+             Log.d("BLUETOOTH", msg);
+             }
+             }
+             }; **/
+
+            final BluetoothAdapter.LeScanCallback mLeScanCallback =  new BluetoothAdapter.LeScanCallback() {
+                @Override
+                public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+                    String msg = device.getName() + "\n" + device.getAddress() + "\n" + rssi;
+                    Log.d("BLUETOOTH", msg);
+                }
+            };
+
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    BTAdapter.stopLeScan(mLeScanCallback);
+                }
+            },1000);
+
+            BTAdapter.startLeScan(mLeScanCallback);
+
+            /** min SDK level 21...
+             BTAdapter.getBluetoothLeScanner().startScan(new ScanCallback() {... }
+             });**/
+
+
+        };
     }
 
     @Override
