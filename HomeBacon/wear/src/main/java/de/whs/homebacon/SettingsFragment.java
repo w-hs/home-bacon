2c19ecc3d1bb9b424f20aa615e8434d016ef2f54
Daniel <danielhardes@gmail.com>
2015-12-08 14:09:35 +0100

    fix

--- HomeBacon/wear/src/main/java/de/whs/homebacon/SettingsFragment.java
+++ HomeBacon/wear/src/main/java/de/whs/homebacon/SettingsFragment.java
@@ -23,7 +23,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import de.whs.homebaconcore.BeaconListener;
 import de.whs.homebaconcore.BeaconScanner;
@@ -47,6 +51,37 @@ public class SettingsFragment extends CardFragment {
     TextView mScannerView;
     private int mScanCount;
     private BeaconListener mListener;
+
+
+
+
+    private class Scan {
+        private int rssi;
+        private long timestamp;
+
+        public Scan(int rssi, long timestamp) {
+            this.rssi = rssi;
+            this.timestamp = timestamp;
+        }
+
+        public int getRssi() {
+            return rssi;
+        }
+
+        public void setRssi(int rssi) {
+            this.rssi = rssi;
+        }
+
+        public long getTimestamp() {
+            return timestamp;
+        }
+
+        public void setTimestamp(long timestamp) {
+            this.timestamp = timestamp;
+        }
+    }
+
+    private Map<String, Scan> scans = new HashMap<>();
 
     @Override
     public View onCreateContentView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -90,13 +125,32 @@ public class SettingsFragment extends CardFragment {
 
 
 
-        Button startButton = (Button)  mRootView.findViewById(R.id.button);
+        final Button startButton = (Button)  mRootView.findViewById(R.id.button);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mIsScanning = true;
-               // mScannerView.setText("Scanner: An");
-                mScanCount = 0;
+                if(mIsScanning)
+                {
+                    startButton.setText("start");
+                    mIsScanning = false;
+                    mScannerView.setText("Scanner: Aus");
+                    File downloadDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                    File destFile = new File(downloadDir, "bacon.db");
+                    File srcFile = new File(getDatabaseDir());
+                    try {
+                        copy(srcFile, destFile);
+                    }
+                    catch (Exception ex) {
+                        Log.e("HomeBeacon", ex.getMessage());
+                    }
+                }
+                else
+                {
+                    startButton.setText("stop");
+                    mIsScanning = true;
+                    mScannerView.setText("Scanner: An");
+                    mScanCount = 0;
+                }
             }
         });
 
@@ -119,7 +173,7 @@ public class SettingsFragment extends CardFragment {
 
 
 
-        if (savedInstanceState == null) {
+        if (mBeaconScanner == null) {
             startBeaconScan();
         } else {
             mIsScanning = savedInstanceState.getBoolean("isScanning");
@@ -132,6 +186,13 @@ public class SettingsFragment extends CardFragment {
             Log.i("HomeBeacon", "Scanner: " + mBeaconScanner);
         }
 
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                onSaveScans();
+            }
+        }, 0, 1100);
+
 
         return mRootView;
     }
@@ -139,7 +200,6 @@ public class SettingsFragment extends CardFragment {
     public void setAdapter(NotesGridPagerAdapter adapter) {
         this.adapter = adapter;
     }
-
 
     private void startBeaconScan() {
         mBeaconScanner = new BeaconScanner(this.getActivity());
@@ -151,10 +211,11 @@ public class SettingsFragment extends CardFragment {
         };
         mBeaconScanner.register(mListener);
     }
-/*
+
+
     private String getDatabaseDir() {
-        PackageManager m = getPackageManager();
-        String s = getPackageName();
+        PackageManager m = this.getActivity().getPackageManager();
+        String s = this.getActivity().getPackageName();
         try {
             PackageInfo p = m.getPackageInfo(s, 0);
             return p.applicationInfo.dataDir + "/databases/bacon.db";
@@ -162,7 +223,7 @@ public class SettingsFragment extends CardFragment {
             return "";
         }
     }
-*/
+
     private void copy(File src, File dst) throws IOException {
         InputStream in = new FileInputStream(src);
         OutputStream out = new FileOutputStream(dst);
@@ -182,12 +243,60 @@ public class SettingsFragment extends CardFragment {
         if (!mIsScanning)
             return;
 
-        Room room = (Room) mSpinner.getSelectedItem();
-        Log.i("HomeBeacon", "room=" + room.getName() + ", addr=" + device.getAddress() + ", rssi="
-                + rssi);
-        ++mScanCount;
-        mScannerView.setText("Scanner: An (" + room.getName() + ": " + mScanCount + ")");
-        mDbHelper.insertScan(mDb, room.getId(), device.getAddress(), rssi);
+        long timestamp = System.currentTimeMillis();
+        Scan scan = scans.get(device.getAddress());
+        if (scan == null) {
+            scans.put(device.getAddress(), new Scan(rssi, timestamp));
+        }
+        else {
+            scan.setRssi(rssi);
+            scan.setTimestamp(timestamp);
+        }
     }
+
+    private boolean hasScans(long fadeLimit) {
+        for (Scan scan : scans.values()) {
+            if (scan.getTimestamp() > fadeLimit) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void onSaveScans() {
+        if (!mIsScanning)
+            return;
+
+        // Wenn wir länger als 2 Sekunden nichts mehr vom Beacon gehört haben,
+        // gehen wir davon aus, dass der Beacon außer Reichweite ist
+        long fadeDurationInMs = 2000;
+        long currentTime = System.currentTimeMillis();
+        long fadeLimit = currentTime - fadeDurationInMs;
+
+        if (!hasScans(fadeLimit))
+            return;
+
+        final Room room = (Room) mSpinner.getSelectedItem();
+        long scanId = mDbHelper.insertScan(mDb, room.getId());
+
+        ++mScanCount;
+        this.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mScannerView.setText("Scanner: An (" + room.getName() + ": " + mScanCount + ")");
+            }
+        });
+
+        for (String address : scans.keySet()) {
+            Scan scan = scans.get(address);
+            if (scan.getTimestamp() > fadeLimit) {
+                mDbHelper.insertScannedTag(mDb, scanId, address, scan.getRssi());
+                Log.i("HomeBeacon", "room=" + room.getName() + ", addr=" + address + ", rssi="
+                        + scan.getRssi());
+            }
+        }
+
+    }
+
 
 }
