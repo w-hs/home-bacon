ce82c5f2dcb9dd813e5eb9b0c113148957b85ab7
MillerDennis <miller.dennis4711@gmail.com>
2015-12-16 10:49:40 +0100

    FragmentLayout in WearApp geändert. Settings ist über den Notes

--- HomeBacon/wear/src/main/java/de/whs/homebacon/MyDisplayActivity.java
+++ HomeBacon/wear/src/main/java/de/whs/homebacon/MyDisplayActivity.java
@@ -1,26 +1,19 @@
 package de.whs.homebacon;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.wearable.view.GridViewPager;
-import android.util.Log;
-import android.widget.TextView;
 
 import java.util.List;
 
-import de.whs.homebaconcore.Constants;
 import de.whs.homebaconcore.DatabaseHelper;
 import de.whs.homebaconcore.Note;
-import de.whs.homebaconcore.PhoneListener;
 
 public class MyDisplayActivity extends Activity {
 
+    private GridViewPager mPager;
     private NotesGridPagerAdapter mNotesAdapter;
 
 
@@ -35,16 +28,19 @@ public class MyDisplayActivity extends Activity {
 
         //Create noteAdapter
         mNotesAdapter = new NotesGridPagerAdapter(this, getFragmentManager());
-        final GridViewPager pager = (GridViewPager) findViewById(R.id.pager);
-        pager.setAdapter(mNotesAdapter);
+        mPager = (GridViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(mNotesAdapter);
 
         updateCards();
+        mPager.setCurrentItem(1, 0);
+
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         updateCards();
+        mPager.setCurrentItem(1, 0);
     }
 
     private void updateCards(){
@@ -54,7 +50,6 @@ public class MyDisplayActivity extends Activity {
         mNotesAdapter.clear();
         List<Note> notes = mDbHelper.getAllNotes(mDb,0); //TODO current room
         mNotesAdapter.addNotes(notes);
-        mNotesAdapter.notifyDataSetChanged();
 
         mDb.close();
     }
--- HomeBacon/wear/src/main/java/de/whs/homebacon/NotesGridPagerAdapter.java
+++ HomeBacon/wear/src/main/java/de/whs/homebacon/NotesGridPagerAdapter.java
@@ -17,14 +17,10 @@ public class NotesGridPagerAdapter extends FragmentGridPagerAdapter {
 
     private final List<Note> mNotes = new ArrayList<Note>();
     private final Context mContext;
-    private final CardFragment defaultFragment = CardFragment.create("keine notizen","");
-    private final CardFragment settingsFragment = new SettingsFragment();
 
     public NotesGridPagerAdapter(Context ctx, FragmentManager fm) {
         super(fm);
         mContext = ctx;
-
-        mNotes.add(new Note("s",""));
     }
 
     public void addNote(Note note) {
@@ -33,56 +29,49 @@ public class NotesGridPagerAdapter extends FragmentGridPagerAdapter {
     }
 
     public void removeNote(Note note) {
-
-        if(mNotes.size()!=1)
         mNotes.remove(note);
+        notifyDataSetChanged();
     }
 
     public void addNotes(List<Note> notes) {
-        mNotes.addAll(0,notes);
+        mNotes.addAll(0, notes);
+        notifyDataSetChanged();
     }
 
     @Override
-    public Fragment getFragment(int row, int col) {
-        if (mNotes.size() == 0) {
+    public Fragment getFragment(int row, int col) throws IndexOutOfBoundsException {
 
-            return defaultFragment;
-        }
-        else
-        {
-
-
+        //row = 0 -> settings fragment
+        if (row==0){
+            return new SettingsFragment();
         }
 
-        if(mNotes.get(col).getTitle()=="s")
-        {
+        //row = 1 -> note fragments
+        if (row == 1) {
+            //no notes -> No
+            if (mNotes.size() == 0) {
+                return CardFragment.create("Keine Notizen","");
+            }
+            else {
+                NoteFragment f = new NoteFragment();
+                Bundle b =new Bundle();
 
-            return settingsFragment;
+                b.putSerializable("note", mNotes.get(col));
+                f.setArguments(b);
+                f.setAdapter(this);
+                f.setContext(mContext);
+
+                return f;
+            }
         }
 
-        //CardFragment f = CardFragment.create("sdfsdf", "dfgdfgdf dfgdfgdf gdfgdfg dfgdfgdfg dfgdfgdf gdfgdfgdfg dfgd");
-
-        NoteFragment f = new NoteFragment();
-        Bundle b =new Bundle();
-
-        b.putSerializable("note", mNotes.get(col));
-        f.setArguments(b);
-        f.setAdapter(this);
-        f.setContext(mContext);
-
-        return f;
+        throw new IndexOutOfBoundsException("Fragment row " + row + " or col " + col + " is not available");
     }
-
-    // Obtain the background image for the row
-   // @Override
-   // public Drawable getBackgroundForRow(int row) {
-   //     return mContext.getResources().getDrawable(R.drawable.bg);
-   // }
 
     // Obtain the number of pages (vertical)
     @Override
     public int getRowCount() {
-        return 1;
+        return 2;
     }
 
     // Obtain the number of pages (horizontal)
@@ -94,4 +83,6 @@ public class NotesGridPagerAdapter extends FragmentGridPagerAdapter {
     public void clear() {
         mNotes.clear();
     }
+
+
 }
